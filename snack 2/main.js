8b70b6c6641e89c482a997be31ec9a5d51dc87ec
AlessP93 <alespiscino@libero.it>
2022-05-04 11:47:16 +0200

    snack 2

---
+++ snack 2/main.js
@@ -0,0 +1,9 @@
+const nameOne = prompt("Inserisci un nome?");
+const nametwo = prompt("Inserisci un nome?");
+console.log(nameOne, nametwo)
+
+if (nameOne > nameOne) {
+    console.log(nameOne);
+} else {
+    console.log(nametwo);
+}
