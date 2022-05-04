f9e9e269ee75ce6cd2082f6e7204cc35188bdb8a
AlessP93 <alespiscino@libero.it>
2022-05-04 18:15:23 +0200

    snack 6

---
+++ snack 6/main.js
@@ -0,0 +1,22 @@
+// Generatore di "nomi cognomi" casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, 
+// e da queste vuole generare una falsa lista di invitati con nome e cognome.
+
+// creo una lista vuota
+const fullName = [];
+
+// genero nomi e cognomi
+const nomi = ["ale", "matteo","luigi","simone"];
+const cognomi = ["rossi", "bianchi","verdi","gialli"];
+console.log(fullName.length)
+
+// genero un nome e cognome random
+for (let i = 0; i < 4; i++) {
+    
+    const nomeRandom = Math.floor(Math.random() * 4);
+    const cognomeRandom = Math.floor(Math.random() * 4);
+    
+    console.log(nomi[nomeRandom], cognomi[cognomeRandom]);
+    
+}
+
+   
