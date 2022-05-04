5a02d066696772ad85cd202f3c1ce669727e10c3
AlessP93 <alespiscino@libero.it>
2022-05-04 12:34:37 +0200

    correzione

--- snack 3/main.js
+++ snack 3/main.js
@@ -1,17 +1,11 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero
 // Il programma stampa la somma di tutti i numeri inseriti
-
+let numero = number
+console.log(numero)
 // 1. chiedo 10 volte all utente di inserire un numero
-const number1 = prompt("Inserisci un numero?");
-const number2 = prompt("Inserisci un numero?");
-const number3 = prompt("Inserisci un numero?");
-const number4 = prompt("Inserisci un numero?");
-const number5 = prompt("Inserisci un numero?");
-const number6 = prompt("Inserisci un numero?");
-const number7 = prompt("Inserisci un numero?");
-const number8 = prompt("Inserisci un numero?");
-const number9 = prompt("Inserisci un numero?");
-const number10 = prompt("Inserisci un numero?");
+for (i = 0; i <= 10; i++) {
+    numero = prompt("inserisci un numero")
+}
 
 // Il programma stampa la somma di tutti i numeri inseriti
-console.log( number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9+ number10)
+console.log(number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9+ number10);
