a70dcc205cc7b74577ef5ae5e2abbf5ccb4ea192
AlessP93 <alespiscino@libero.it>
2022-05-05 17:50:13 +0200

    fix snack 5

--- snack 5/main.js
+++ snack 5/main.js
@@ -1,12 +1,11 @@
 // Inserisci un numero
 let numero = Number(promt("Inserisci un numero"));
 
-
 // SE è pari stampa il numero
 if (numero % 2 === 0) {
     console.log(numero);
 // ALTRIMENTI se è dispari stampa il numero successivo
 } else {
     numero ++;
-    console.log(numero)
+    console.log(numero) // o anche console.log(numero) + 1
 }
